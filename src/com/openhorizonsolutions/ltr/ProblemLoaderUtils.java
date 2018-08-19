301a17f47c28dbdce4aa8b1bda64567f3827628f
_c0da_ <duvictor514@gmail.com>
2018-08-18 23:17:48 -0700

    Make sure examples are functional

--- src/com/openhorizonsolutions/ltr/ProblemLoaderUtils.java
+++ src/com/openhorizonsolutions/ltr/ProblemLoaderUtils.java
@@ -7,11 +7,17 @@ public class ProblemLoaderUtils
 	// the folowing methods are sample methods that return dummy output.
 	// these will return dynamic data based on reading config files from a designated directory later.
 	private static ArrayList<Problem> tmpLst = new ArrayList<Problem>();
-	boolean blooean = tmpLst.add(new Problem("1", "Sample Problem OwO", "Test Contest Set Mark IV", "(insert PDF view here + notes + html code)", null, null));
-	boolean blooean2 = tmpLst.add(new Problem("2", "Sample Problem OwO II", "Test Contest Set Mark IV", "(insert PDF view here + notes + html code)", null, null));
+		
+	public static void refreshIO()
+	{
+		tmpLst = new ArrayList<Problem>();
+		tmpLst.add(new Problem("1", "Sample Problem OwO", "Test Contest Set Mark IV", "(insert PDF view here + notes + html code)", null, null));
+		tmpLst.add(new Problem("2", "Sample Problem OwO II", "Test Contest Set Mark IV", "(insert PDF view here + notes + html code)", null, null));
+	}
 	
 	public static boolean problemExists(String cpid)
 	{
+		refreshIO();
 		System.out.println("CPID:" + cpid);
 		for (Problem p : tmpLst)
 		{
