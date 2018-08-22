131c8c2f9d1d5fe34bf86973b9703bc20ea5d920
_c0da_ <duvictor514@gmail.com>
2018-08-22 12:23:21 -0700

    Removed strict language requirements

--- src/com/openhorizonsolutions/ltr/ProblemServlet.java
+++ src/com/openhorizonsolutions/ltr/ProblemServlet.java
@@ -373,11 +373,11 @@ public class ProblemServlet extends HttpServlet
 						"			<div class=\"field2\">\n" + 
 						"				<label for=\"language\">Language:</label>\n" + 
 						"				<select name=\"language\">\n" + 
-						"					<option value='1'>C (GCC 8.1)</option>\n" + 
-						"					<option value='2'>C++ (GCC 8.1)</option>\n" + 
-						"					<option value='3'>Java (Oracle JVM 8)</option>\n" + 
-						"					<option value='4'>Python (2.7.6)</option>\n" + 
-						"					<option value='5'>Python (3.4.0)</option>\n" + 
+						"					<option value='1'>C</option>\n" + 
+						"					<option value='2'>C++11</option>\n" + 
+						"					<option value='3'>Java</option>\n" + 
+						"					<option value='4'>Python 2</option>\n" + 
+						"					<option value='5'>Python 3</option>\n" + 
 						"				</select></div>\n" + 
 						"			\n" + 
 						"			<div class=\"field2\">\n" + 
